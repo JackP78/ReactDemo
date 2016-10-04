f394e9cdbb55abeb2eb4bb34ee8f75faf293255f
Jack McAuliffe <jack.mcauliffe01@gmail.com>
2016-10-04 15:54:03 +0100

    tutorial 6

--- src/js/client.js
+++ src/js/client.js
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router"
 
-import Layout from "./components/Layout";
+import Layout from "./pages/Layout";
+import Featured from "./pages/Featured";
+import Archive from "./pages/Archive";
+import Settings from "./pages/Settings";
 
 const app = document.getElementById('app');
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={Layout}/>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path="archives" component={Archive}></Route>
+      <Route path="settings" component={Settings}></Route>
+    </Route>
   </Router>
   , app);
