ec5babc85c9a0829b646fa41f1c303e1f0e7511b
Jack McAuliffe <jack.mcauliffe01@gmail.com>
2016-10-04 14:43:24 +0100

    getting ready for router example

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,7 @@
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import hello from "./components/hello"
+import Layout from "./components/Layout";
 
-const app = document.getElementById('hello')
-ReactDOM.render(<Hello/>, app);
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
