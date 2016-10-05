6aea0b476275df6dca206b5af0742dac1923d191
Jack McAuliffe <jack.mcauliffe01@gmail.com>
2016-10-05 11:48:40 +0100

    remove buttons when status not awaiting approval

--- src/js/components/Task.js
+++ src/js/components/Task.js
@@ -21,18 +21,34 @@ export default class Task extends React.Component {
     });
   }
 
+  getButtons() {
+    return (
+      <div>
+        <a class="btn btn-default" href="#" onClick={this.acceptTask.bind(this)}>Accept</a>
+        <a class="btn btn-default" href="#" onClick={this.rejectTask.bind(this)}>Reject</a>
+      </div>
+    )
+  }
 
 
   render() {
     const { title } = this.props;
-
+    const { status } = this.state;
+    const awaitingApproval = (status == "awaiting approval");
+    var buttons = null;
+    var statusDiv = null;
+    if (awaitingApproval) {
+      buttons = this.getButtons()
+    }
+    else {
+      statusDiv = <h5>Status: {this.state.status}</h5>
+    }
     return (
       <div class="col-md-4">
         <h4>{title}</h4>
-        <h5>Status: {this.state.status}</h5>
+        {statusDiv}
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-        <a class="btn btn-default" href="#" onClick={this.acceptTask.bind(this)}>Accept</a>
-        <a class="btn btn-default" href="#" onClick={this.rejectTask.bind(this)}>Reject</a>
+        {buttons}
       </div>
     );
   }
