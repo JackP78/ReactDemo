0d36409c89f55f2a94712cffc30c24f51231144a
Jack McAuliffe <jack.mcauliffe01@gmail.com>
2016-10-05 11:34:22 +0100

    accept task

--- src/js/components/Task.js
+++ src/js/components/Task.js
@@ -1,16 +1,38 @@
 import React from "react";
 
 export default class Task extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      status: "awaiting approval",
+    };
+  }
+
+
+  acceptTask() {
+    this.setState({
+      status: "accepted",
+    });
+  }
+
+  rejectTask() {
+    this.setState({
+      status: "rejected",
+    });
+  }
+
+
+
   render() {
     const { title } = this.props;
 
     return (
       <div class="col-md-4">
         <h4>{title}</h4>
-        <h5>Status: waiting approval</h5>
+        <h5>Status: {this.state.status}</h5>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-        <a class="btn btn-default" href="#">Accept</a>
-        <a class="btn btn-default" href="#">Reject</a>
+        <a class="btn btn-default" href="#" onClick={this.acceptTask.bind(this)}>Accept</a>
+        <a class="btn btn-default" href="#" onClick={this.rejectTask.bind(this)}>Reject</a>
       </div>
     );
   }
